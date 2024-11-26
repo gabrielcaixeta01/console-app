3693670bdc5605cefbf1395651c5a03359aebb7a
gabrielcaixeta01 <gabrielcaixeta@Laptop-de-Gabriel.local>
2024-11-26 11:10:42 -0300

    arrumando o main torto do joao

--- main.cpp
+++ main.cpp
@@ -5,98 +5,94 @@
 
 using namespace std;
 
-int main()
-{
-    TUAvaliacao testeAvaliacao
-
-    switch(testeAvaliacao.run()){
-            case TUAvaliacao::SUCESSO: cout<<"SUCESSO"<<endl;
-            break
-    case TUAvaliacao::FALHA : cout<<"FALHA"<<endl;
-        break;
+int main() {
+    // Teste de Avaliação
+    TUAvaliacao testeAvaliacao;
+    switch (testeAvaliacao.run()) {
+        case TUAvaliacao::SUCESSO:
+            cout << "Teste Avaliacao: SUCESSO" << endl;
+            break;
+        case TUAvaliacao::FALHA:
+            cout << "Teste Avaliacao: FALHA" << endl;
+            break;
     }
-return 0;
-}
 
-{
-    TUCodigo testeCodigo
-
-    switch(testeCodigo.run()){
-            case TUCodigo::SUCESSO: cout<<"SUCESSO"<<endl;
-            break
-    case TUCodigo::FALHA : cout<<"FALHA"<<endl;
-        break;
+    // Teste de Código
+    TUCodigo testeCodigo;
+    switch (testeCodigo.run()) {
+        case TUCodigo::SUCESSO:
+            cout << "Teste Codigo: SUCESSO" << endl;
+            break;
+        case TUCodigo::FALHA:
+            cout << "Teste Codigo: FALHA" << endl;
+            break;
     }
-return 0;
-}
 
-{
-    TUData testeData
-
-    switch(testeData.run()){
-            case TUData::SUCESSO: cout<<"SUCESSO"<<endl;
-            break
-    case TUData::FALHA : cout<<"FALHA"<<endl;
-        break;
+    // Teste de Data
+    TUData testeData;
+    switch (testeData.run()) {
+        case TUData::SUCESSO:
+            cout << "Teste Data: SUCESSO" << endl;
+            break;
+        case TUData::FALHA:
+            cout << "Teste Data: FALHA" << endl;
+            break;
     }
-return 0;
-}
 
-{
-    TUDinheiro testeDinheiro
-    switch(testeDinheiro.run()){
-            case TUDinheiro::SUCESSO: cout<<"SUCESSO"<<endl;
-            break
-    case TUDinheiro::FALHA : cout<<"FALHA"<<endl;
-        break;
+    // Teste de Dinheiro
+    TUDinheiro testeDinheiro;
+    switch (testeDinheiro.run()) {
+        case TUDinheiro::SUCESSO:
+            cout << "Teste Dinheiro: SUCESSO" << endl;
+            break;
+        case TUDinheiro::FALHA:
+            cout << "Teste Dinheiro: FALHA" << endl;
+            break;
     }
-return 0;
-}
 
-{
-    TUDuracao testeDuracao
-
-    switch(testeDuracao.run()){
-            case TUDuracao::SUCESSO: cout<<"SUCESSO"<<endl;
-            break
-    case TUDuracao::FALHA : cout<<"FALHA"<<endl;
-        break;
+    // Teste de Duração
+    TUDuracao testeDuracao;
+    switch (testeDuracao.run()) {
+        case TUDuracao::SUCESSO:
+            cout << "Teste Duracao: SUCESSO" << endl;
+            break;
+        case TUDuracao::FALHA:
+            cout << "Teste Duracao: FALHA" << endl;
+            break;
     }
-return 0;
-}
 
-{
-    TUHorario testeHorario
-
-    switch(testeHorario.run()){
-            case TUHorario::SUCESSO: cout<<"SUCESSO"<<endl;
-            break
-    case TUHorario::FALHA : cout<<"FALHA"<<endl;
-        break;
+    // Teste de Horário
+    TUHorario testeHorario;
+    switch (testeHorario.run()) {
+        case TUHorario::SUCESSO:
+            cout << "Teste Horario: SUCESSO" << endl;
+            break;
+        case TUHorario::FALHA:
+            cout << "Teste Horario: FALHA" << endl;
+            break;
     }
-return 0;
-}
 
-{
-    TUNome testeNome
-
-    switch(testeNome.run()){
-            case TUNome::SUCESSO: cout<<"SUCESSO"<<endl;
-            break
-    case TUNome::FALHA : cout<<"FALHA"<<endl;
-        break;
+    // Teste de Nome
+    TUNome testeNome;
+    switch (testeNome.run()) {
+        case TUNome::SUCESSO:
+            cout << "Teste Nome: SUCESSO" << endl;
+            break;
+        case TUNome::FALHA:
+            cout << "Teste Nome: FALHA" << endl;
+            break;
     }
-return 0;
-}
 
-{
-    TUSenha testeSenha
-
-    switch(testeSenha.run()){
-            case TUSenha::SUCESSO: cout<<"SUCESSO"<<endl;
-            break
-    case TUSenha::FALHA : cout<<"FALHA"<<endl;
-        break;
+    // Teste de Senha
+    TUSenha testeSenha;
+    switch (testeSenha.run()) {
+        case TUSenha::SUCESSO:
+            cout << "Teste Senha: SUCESSO" << endl;
+            break;
+        case TUSenha::FALHA:
+            cout << "Teste Senha: FALHA" << endl;
+            break;
     }
-return 0;
-}
+
+    return 0;
+};
